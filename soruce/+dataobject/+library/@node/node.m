1ed25dfba65e84b3249d166c7935b866cb0c2843
diogtsix <diogtsix@gmail.com>
2023-07-23 18:52:15 +0200

    Creating node class.

--- soruce/+dataobject/+library/@node/node.m
+++ soruce/+dataobject/+library/@node/node.m
@@ -1,23 +1,41 @@
 classdef node
-    %NODE Summary of this class goes here
-    %   Detailed explanation goes here
+    %NODE Class for each node
     
     properties
-        Property1
+        id
+        x_position
+        y_position
+        z_position
     end
     
     methods
-        function obj = node(inputArg1,inputArg2)
-            %NODE Construct an instance of this class
-            %   Detailed explanation goes here
-            obj.Property1 = inputArg1 + inputArg2;
-        end
-        
-        function outputArg = method1(obj,inputArg)
-            %METHOD1 Summary of this method goes here
-            %   Detailed explanation goes here
-            outputArg = obj.Property1 + inputArg;
+        %Constructor
+        function obj = node(id, x_position, ...
+                y_position,z_position)
+            
+            obj.id = id ;
+            obj.x_position = x_position ;
+            obj.y_position = y_position ;
+            obj.z_position = z_position ;
         end
     end
+    
+    methods (Static)
+        %Define attributes
+        function obj = define(options)
+            arguments
+                options.id (1,1){mustBeReal} = 0;
+                options.x_position (1,1) {mustBeReal} = 0.5;
+                options.y_position (1,1) {mustBeReal} = 0.5;
+                options.z_position (1,1) {mustBeReal} = 0.5;
+                
+            end
+            obj = feval(...
+                options.id, ...
+                options.x_position, ...
+                options.y_position, ...
+                options.z_position);
+        end
+        
+    end
 end
-
